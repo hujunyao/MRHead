e7d3d9292d1ad79bba8b35f71854f3cfc0138a8c
imsityrain <1293830063@qq.com>
2017-10-20 23:53:27 +0800

    Update to work tesseract 4.0

--- mrtesseract.h
+++ mrtesseract.h
@@ -1,27 +1,37 @@
 #ifndef MRTESSERACT_H
 #define MRTESSERACT_H
-#include "allheaders.h"
+#define TESS_WITH_OPENCV 1
 #include "baseapi.h"
 #include "string"
 #ifdef _WIN32
 #if _DEBUG
+#if TESSERACT_VERSION>=0x040000
+#pragma comment(lib,"tesseract400d.lib")
+#else
 #pragma comment(lib,"libtesseract304d.lib")
 #pragma comment(lib,"liblept171d.lib")
+#endif
+#else
+#if TESSERACT_VERSION>=0x040000
+#pragma comment(lib,"tesseract400.lib")
 #else
 #pragma comment(lib,"libtesseract304.lib")
 #pragma comment(lib,"liblept171.lib")
 #endif
 #endif
-#define TESS_WITH_OPENCV 1
+#endif
+
 #if TESS_WITH_OPENCV
 #include "opencv2/opencv.hpp"
+#else
+#include "allheaders.h"
 #endif
 #ifdef USE_ENG_DATA
 const char* langname = "eng";
 #else
 #define TESSERACT_PASSPORT 1
 #if TESSERACT_PASSPORT
-const char* langname = "chi_sim";
+const char* langname = "eng";
 #endif
 #endif
 
@@ -53,7 +63,7 @@ private:
 	tesseract::TessBaseAPI  api;
 	int inited = -1;
 	MRTessNumRecognier(){
-		inited = api.Init("", "eng");
+		inited = api.Init("./", "eng");
 		api.SetVariable("tessedit_char_whitelist", "0123456789");
 		api.SetPageSegMode(tesseract::PSM_SINGLE_CHAR);
 	}
@@ -91,7 +101,7 @@ private:
 	tesseract::TessBaseAPI  api;
 	int inited = -1;
 	MRTessChineseRecognier(){
-		inited=api.Init("", "chi_sim");
+		inited = api.Init("./", "chi_sim");
 		api.SetPageSegMode(tesseract::PSM_SINGLE_CHAR);
 	}
 	~MRTessChineseRecognier()
@@ -108,19 +118,6 @@ public:
 		static MRTesseract instance;
 		return &instance;
 	}
-	std::string recog(std::string filepath)
-	{
-		Pix*  pixs = pixRead(filepath.c_str());
-		if (pixs == NULL)
-		{
-			std::cout << "cannot load " << filepath << std::endl;
-			return "";
-		}
-		api.SetImage(pixs);
-		std::string ret = api.GetUTF8Text();
-		pixDestroy(&pixs);
-		return ret;
-	}
 #if TESS_WITH_OPENCV
 	std::string recog(const cv::Mat &img)
 	{
@@ -136,16 +133,39 @@ public:
 		}
 		return str;
 	}
+	std::string recog(const std::string filepath)
+	{
+		std::string str="";
+		cv::Mat img=cv::imread(filepath);
+		if(!img.data)
+			return str;
+		else
+			return recog(img);
+	}
+#else
+	std::string recog(const std::string filepath)
+	{
+		Pix*  pixs = pixRead(filepath.c_str());
+		if (pixs == NULL)
+		{
+			std::cout << "cannot load " << filepath << std::endl;
+			return "";
+		}
+		api.SetImage(pixs);
+		std::string ret = api.GetUTF8Text();
+		pixDestroy(&pixs);
+		return ret;
+	}
 #endif
 private:
 	MRTesseract()
 	{
 #ifdef USE_ENG_DATA
-		inited=api.Init("", "eng");
+		inited=api.Init("./", "eng");
 		api.SetVariable("tessedit_char_whitelist", "0123456789");
 #else
 #if TESSERACT_PASSPORT
-		api.Init("", langname);
+		inited = api.Init("./", langname);
 #else	
 		api.SetVariable("tessedit_char_whitelist", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ<");
 #endif
